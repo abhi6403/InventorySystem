84efdb3abf0516307a27437f1db57b5f72920808
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2025-03-03 07:46:33 +0530

    Updated Inventory Manager

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -8,21 +8,43 @@ using Image = UnityEngine.UI.Image;
 public class InventoryManager : MonoBehaviour
 {
     public InventoryScriptableObject inventoryScriptableObject;
-    public Transform inventoryContainer;
-    public GameObject itemPrefab;
+    public ItemService itemService;
+    public Transform itemContainer;
 
-    private void Start()
-    {
-        initialize();
-    }
-
-    public void initialize()
+    public void AllInventoryItems()
     {
         for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
         {
-            itemPrefab.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = inventoryScriptableObject.items[i]._amount.ToString();
-            itemPrefab.transform.GetChild(1).GetComponent<Image>().sprite = inventoryScriptableObject.items[i]._sprite;
-            GameObject item = Instantiate(itemPrefab, inventoryContainer);
+            string priceText = inventoryScriptableObject.items[i]._amount;
+            string itemName = inventoryScriptableObject.items[i]._name;
+            string itemDescription = inventoryScriptableObject.items[i]._description;
+            Sprite sprite = inventoryScriptableObject.items[i]._sprite;
+            itemService.Initialize(sprite,itemName,itemDescription,priceText);
+            ItemService itemSer = GameObject.Instantiate(itemService, itemContainer);
+        }
+    }
+    
+    public void SortInventoryItem(ItemTypes itemType)
+    {
+        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
+        {
+            if(inventoryScriptableObject.items[i]._itemType == itemType)
+            {
+                string priceText = inventoryScriptableObject.items[i]._amount;
+                string itemName = inventoryScriptableObject.items[i]._name;
+                string itemDescription = inventoryScriptableObject.items[i]._description;
+                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
+                itemService.Initialize(sprite,itemName,itemDescription,priceText);
+                ItemService itemSer = GameObject.Instantiate(itemService, itemContainer);
+            }
+        }
+    }
+    
+    public void clearAllItems()
+    {
+        foreach (Transform child in itemContainer )
+        {
+            Destroy(child.gameObject);
         }
     }
 }
--- Assets/Scripts/UI/UIService.cs
+++ Assets/Scripts/UI/UIService.cs
@@ -9,166 +9,71 @@ public class UIService : MonoBehaviour
     public InventoryScriptableObject inventoryScriptableObject;
     public Transform inventoryContainer;
     public ItemService itemService;
-
-    public void initialize()
-    {
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            string priceText = inventoryScriptableObject.items[i]._amount;
-            string itemName = inventoryScriptableObject.items[i]._name;
-            string itemDescription = inventoryScriptableObject.items[i]._description;
-            Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-            itemService.Initialize(inventoryScriptableObject.items[i]._sprite,itemName,itemDescription,priceText);
-            ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-        }
-    }
+    
+    public InventoryManager inventoryManager;
+    
+    
 
     public void getAllItems()
     {
-        initialize();
+        inventoryManager.AllInventoryItems();
     }
 
     public void getWeaponItems()
     {
         clearAllItems();
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            if (inventoryScriptableObject.items[i]._itemType == ItemTypes.WEAPON)
-            {
-                string priceText = inventoryScriptableObject.items[i]._amount;
-                string itemName = inventoryScriptableObject.items[i]._name;
-                string itemDescription = inventoryScriptableObject.items[i]._description;
-                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-                itemService.Initialize(sprite,itemName,itemDescription,priceText);
-                ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-            }
-        }
+        
+        inventoryManager.SortInventoryItem(ItemTypes.WEAPON);
+        
     }
 
     public void getFoodItems()
     {
         clearAllItems();
 
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            if (inventoryScriptableObject.items[i]._itemType == ItemTypes.HEALTH)
-            {
-                string priceText = inventoryScriptableObject.items[i]._amount;
-                string itemName = inventoryScriptableObject.items[i]._name;
-                string itemDescription = inventoryScriptableObject.items[i]._description;
-                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-                itemService.Initialize(sprite,itemName,itemDescription,priceText);
-                ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-            }
-        }
+        inventoryManager.SortInventoryItem(ItemTypes.HEALTH);
     }
 
     public void getDevilFruits()
     {
         clearAllItems();
         
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            if (inventoryScriptableObject.items[i]._itemType == ItemTypes.DEVILFRUIT)
-            {
-                string priceText = inventoryScriptableObject.items[i]._amount;
-                string itemName = inventoryScriptableObject.items[i]._name;
-                string itemDescription = inventoryScriptableObject.items[i]._description;
-                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-                itemService.Initialize(sprite,itemName,itemDescription,priceText);
-                ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-            }
-        }
+        inventoryManager.SortInventoryItem(ItemTypes.DEVILFRUIT);
     }
 
     public void getPosters()
     {
         clearAllItems();
         
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            if (inventoryScriptableObject.items[i]._itemType == ItemTypes.POSTER)
-            {
-                string priceText = inventoryScriptableObject.items[i]._amount;
-                string itemName = inventoryScriptableObject.items[i]._name;
-                string itemDescription = inventoryScriptableObject.items[i]._description;
-                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-                itemService.Initialize(sprite,itemName,itemDescription,priceText);
-                ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-            }
-        }
+        inventoryManager.SortInventoryItem(ItemTypes.POSTER);
     }
     
     public void getProps()
     {
         clearAllItems();
         
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            if (inventoryScriptableObject.items[i]._itemType == ItemTypes.PROPS)
-            {
-                string priceText = inventoryScriptableObject.items[i]._amount;
-                string itemName = inventoryScriptableObject.items[i]._name;
-                string itemDescription = inventoryScriptableObject.items[i]._description;
-                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-                itemService.Initialize(sprite,itemName,itemDescription,priceText);
-                ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-            }
-        }
+        inventoryManager.SortInventoryItem(ItemTypes.PROPS);
     }
     
     public void getPotions()
     {
         clearAllItems();
         
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            if (inventoryScriptableObject.items[i]._itemType == ItemTypes.POTION)
-            {
-                string priceText = inventoryScriptableObject.items[i]._amount;
-                string itemName = inventoryScriptableObject.items[i]._name;
-                string itemDescription = inventoryScriptableObject.items[i]._description;
-                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-                itemService.Initialize(sprite,itemName,itemDescription,priceText);
-                ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-            }
-        }
+        inventoryManager.SortInventoryItem(ItemTypes.POTION);
     }
     
     public void getShipItems()
     {
         clearAllItems();
         
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            if (inventoryScriptableObject.items[i]._itemType == ItemTypes.SHIPITEMS)
-            {
-                string priceText = inventoryScriptableObject.items[i]._amount;
-                string itemName = inventoryScriptableObject.items[i]._name;
-                string itemDescription = inventoryScriptableObject.items[i]._description;
-                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-                itemService.Initialize(sprite,itemName,itemDescription,priceText);
-                ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-            }
-        }
+        inventoryManager.SortInventoryItem(ItemTypes.SHIPITEMS);
     }
     
     public void getMap()
     {
         clearAllItems();
         
-        for (int i = 0; i < inventoryScriptableObject.items.Count; i++)
-        {
-            if (inventoryScriptableObject.items[i]._itemType == ItemTypes.MAP)
-            {
-                Sprite sprite = inventoryScriptableObject.items[i]._sprite;
-                string priceText = inventoryScriptableObject.items[i]._amount;
-                string itemName = inventoryScriptableObject.items[i]._name;
-                string itemDescription = inventoryScriptableObject.items[i]._description;
-                itemService.Initialize(sprite,itemName,itemDescription,priceText);
-                ItemService itemSer = GameObject.Instantiate(itemService, inventoryContainer);
-            }
-        }
+        inventoryManager.SortInventoryItem(ItemTypes.MAP);
     }
 
     public void clearAllItems()
